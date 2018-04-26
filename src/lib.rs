f3dd8353e636b7e3f3a4694c1b7b2c79b3dfd304
Alex Sayers <alex.sayers@gmail.com>
2018-04-26 10:49:54 +0900

    Allow caller to specify watched threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-/*! A barrier which blocks until any thread panics.
+/*! A barrier which blocks until a watched thread panics.
 
 ```
 #[macro_use] extern crate lazy_static;
@@ -16,28 +16,38 @@ fn main() {
     // Initialise the PanicBarrier
     PANIC_BARRIER.init();
 
-    // Spawn some threads
-    let x = thread::spawn(|| { thread::sleep(Duration::from_millis(100)); panic!(); });
+    let h1 = thread::spawn(|| {
+        thread::sleep(Duration::from_millis(100));
+        panic!();
+    });
 
-    // Wait until a thread panics
-    PANIC_BARRIER.wait();
+    let h2 = thread::spawn(move || {
+        PANIC_BARRIER.wait(&[h1.thread().id()]);
+        // ^ this will block until thread 1 panicks
+        PANIC_BARRIER.wait(&[h1.thread().id()]);
+        // ^ this will return immediately, since thread 1 is already dead
+    });
+
+    h2.join().unwrap();
 }
 ```
 */
 
+use std::collections::HashSet;
 use std::panic;
+use std::thread::{self, ThreadId};
 use std::sync::*;
 use std::time::*;
 
 pub struct PanicBarrier {
-    lock: Mutex<bool>,
+    panicked: Mutex<HashSet<ThreadId>>,   // Threads which have panicked
     cvar: Condvar,
 }
 
 impl PanicBarrier {
     pub fn new() -> PanicBarrier {
         PanicBarrier {
-            lock: Mutex::new(false),
+            panicked: Mutex::new(HashSet::new()),
             cvar: Condvar::new(),
         }
     }
@@ -47,26 +57,41 @@ impl PanicBarrier {
     pub fn init(&'static self) {
         let hook = panic::take_hook();
         panic::set_hook(Box::new(move|x| {
-            let mut panicking = self.lock.lock().unwrap();
-            *panicking = true;
+            let mut panicked = self.panicked.lock().unwrap();
+            panicked.insert(thread::current().id());
             self.cvar.notify_all();
             hook(x);
         }));
     }
 
     /// Block the current thread until some other thread panics.
-    pub fn wait(&self) {
-        let mut panicking = self.lock.lock().unwrap();
-        while !*panicking { panicking = self.cvar.wait(panicking).unwrap(); }
+    pub fn wait(&self, watch_list: &[ThreadId]) -> Vec<ThreadId> {
+        let mut panicked = self.panicked.lock().unwrap();
+
+        let watch_list: HashSet<ThreadId> = watch_list.into_iter().map(|x|x.clone()).collect();
+        let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked).map(|x|x.clone()).collect();
+        if watched_panicked.len() > 0 { return watched_panicked; }
+
+        loop {
+            panicked = self.cvar.wait(panicked).unwrap();
+            let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked).map(|x|x.clone()).collect();
+            if watched_panicked.len() > 0 { return watched_panicked; }
+        }
     }
 
     /// Block the current thread until some other thread panics.
-    pub fn wait_timeout(&self, dur: Duration) -> WaitTimeoutResult {
-        let mut panicking = self.lock.lock().unwrap();
+    pub fn wait_timeout(&self, watch_list: &[ThreadId], dur: Duration) -> Vec<ThreadId> {
+        let mut panicked = self.panicked.lock().unwrap();
+
+        let watch_list: HashSet<ThreadId> = watch_list.into_iter().map(|x|x.clone()).collect();
+        let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked).map(|x|x.clone()).collect();
+        if watched_panicked.len() > 0 { return watched_panicked; }
+
         loop {
-            let (guard, res) = self.cvar.wait_timeout(panicking, dur).unwrap();
-            panicking = guard;
-            if *panicking || res.timed_out() { return res; }
+            let (guard, res) = self.cvar.wait_timeout(panicked, dur).unwrap();
+            panicked = guard;
+            let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked).map(|x|x.clone()).collect();
+            if watched_panicked.len() > 0 || res.timed_out() { return watched_panicked; }
         }
     }
 }
