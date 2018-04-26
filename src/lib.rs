9e87422a452daec8358eeaceeb9dd189b8c64536
Alex Sayers <alex.sayers@gmail.com>
2018-04-26 15:19:19 +0900

    Return threads, not just the id

--- src/lib.rs
+++ src/lib.rs
@@ -33,23 +33,23 @@ fn main() {
 ```
 */
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::panic;
 use std::sync::*;
-use std::thread::{self, ThreadId};
+use std::thread::{self, Thread, ThreadId};
 use std::time::*;
 
 const POISON_MSG: &str = "panic_barrier: Inner lock poisoned (please submit a bug report)";
 
 pub struct PanicBarrier {
-    panicked: Mutex<HashSet<ThreadId>>,   // All threads which have historically panicked
+    panicked: Mutex<HashMap<ThreadId, Thread>>,   // All threads which have historically panicked
     cvar: Condvar,
 }
 
 impl PanicBarrier {
     pub fn new() -> PanicBarrier {
         PanicBarrier {
-            panicked: Mutex::new(HashSet::new()),
+            panicked: Mutex::new(HashMap::new()),
             cvar: Condvar::new(),
         }
     }
@@ -60,7 +60,8 @@ impl PanicBarrier {
         let hook = panic::take_hook();
         panic::set_hook(Box::new(move|x| {
             let mut panicked = self.panicked.lock().expect(POISON_MSG);
-            panicked.insert(thread::current().id());
+            let current = thread::current();
+            panicked.insert(current.id(), current);
             self.cvar.notify_all();
             hook(x);
         }));
@@ -72,14 +73,15 @@ impl PanicBarrier {
     /// Note that this function returns as soon as one or more of the threads on the watch list has
     /// panicked.  This means that if you specify a thread which has already panicked, this
     /// function will return immediately.  Think of it as level-triggered, not edge-triggered.
-    pub fn wait(&self, watch_list: &[ThreadId]) -> Vec<ThreadId> {
-        let watch_list: HashSet<ThreadId> = watch_list.into_iter()
-                .map(|x| x.clone()).collect();
+    pub fn wait(&self, watch_list: &[ThreadId]) -> Vec<Thread> {
+        let mut watched_panicked = vec![];
         let mut panicked = self.panicked.lock().expect(POISON_MSG);
-
         loop {
-            let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked)
-                    .map(|x| x.clone()).collect();
+            for tid in watch_list {
+                if let Some(t) = panicked.get(tid) {
+                    watched_panicked.push(t.clone());
+                }
+            }
             if watched_panicked.len() > 0 { return watched_panicked; }
             panicked = self.cvar.wait(panicked).expect(POISON_MSG);
         }
@@ -91,14 +93,15 @@ impl PanicBarrier {
     /// Note that this function returns as soon as one or more of the threads on the watch list has
     /// panicked.  This means that if you specify a thread which has already panicked, this
     /// function will return immediately.  Think of it as level-triggered, not edge-triggered.
-    pub fn wait_timeout(&self, watch_list: &[ThreadId], dur: Duration) -> Vec<ThreadId> {
-        let watch_list: HashSet<ThreadId> = watch_list.into_iter()
-                .map(|x| x.clone()).collect();
+    pub fn wait_timeout(&self, watch_list: &[ThreadId], dur: Duration) -> Vec<Thread> {
+        let mut watched_panicked = vec![];
         let mut panicked = self.panicked.lock().expect(POISON_MSG);
-
         loop {
-            let watched_panicked: Vec<ThreadId> = watch_list.intersection(&panicked)
-                    .map(|x| x.clone()).collect();
+            for tid in watch_list {
+                if let Some(t) = panicked.get(tid) {
+                    watched_panicked.push(t.clone());
+                }
+            }
             if watched_panicked.len() > 0 { return watched_panicked; }
             let (guard, res) = self.cvar.wait_timeout(panicked, dur).expect(POISON_MSG);
             panicked = guard;
@@ -108,10 +111,14 @@ impl PanicBarrier {
 
     /// Check if any of the specified threads have panicked.  This function will not normally
     /// block.
-    pub fn check(&self, watch_list: &[ThreadId]) -> Vec<ThreadId> {
-        let watch_list: HashSet<ThreadId> = watch_list.into_iter()
-                .map(|x| x.clone()).collect();
+    pub fn check(&self, watch_list: &[ThreadId]) -> Vec<Thread> {
+        let mut watched_panicked = vec![];
         let panicked = self.panicked.lock().expect(POISON_MSG);
-        watch_list.intersection(&panicked).map(|x| x.clone()).collect()
+        for tid in watch_list {
+            if let Some(t) = panicked.get(tid) {
+                watched_panicked.push(t.clone());
+            }
+        }
+        watched_panicked
     }
 }
